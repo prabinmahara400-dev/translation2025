80777bfa64d36cf935d28a1fd3f7c079d1c9a37a
prabinmahara400-dev <prabinmahara400@gmail.com>
2025-11-20 05:56:02 +0000

    完成

--- app.py
+++ app.py
@@ -11,14 +11,15 @@ model = genai.GenerativeModel("gemini-flash-latest")
 st.title("みんなでつくるAI翻訳アプリ") # タイトル
 
 source_text = st.text_area("翻訳したいテキストを入力してください") # テキスト入力欄
-target_lang = st.selectbox("翻訳先の言語", []) # セレクトボックス
+target_lang = st.selectbox("翻訳先の言語", ["ネパール"]) # セレクトボックス
 submit_button = st.button("〇〇〇") # ボタン
 
 # --- ボタンが押された後の処理 ---
 if submit_button and source_text:
     # Geminiへの命令文を作成
-    
-    # APIを呼び出し
-    
+    #prompt =f"{target_lang}選択してください:/n{siourse}"
+     # APIを呼び出し
+    response = model.genarate_content(prompt)
     # 結果を表示
     st.subheader("翻訳結果")
+    st.write(response.taxt)
